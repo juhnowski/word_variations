b03b8d77071a801d3bf7fc88f2f2e528442a3b3f
juhnowski <juhnowski@gmail.com>
2017-03-12 01:26:49 +0300

    first commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+HOW TO USE
+==========
+Call function with word for variation: getVariants(word)
+Function returns array of variants.
+If you wish to change algorithm feel be free to change array of char's variants in the code: transitionTable
---
+++ search_var.js
@@ -0,0 +1,91 @@
+var searchList;
+
+function doVariant(pref, word) {
+
+	if (pref.length == 0) {
+		searchList.push(word);
+	}
+
+	if (word.length==0) {
+		return;
+	 }
+
+	var varChar = getChar(word);
+
+	if (word.length > 1) {
+		doVariant(pref + word[0], word.substring(1));	
+	}
+
+	transitionTable.forEach(function(variants) {
+    	var cases = [];
+    	var isExist = false;
+    	
+    	variants.forEach(function(variant) {
+    		if (variant == varChar) {
+    			isExist = true;
+    		} else {
+    			cases.push(variant);
+    		}
+		});
+
+    	if (isExist == true) {
+    		cases.forEach(function(newChar) {
+    			searchList.push(pref + newChar + word.substring(varChar.length));
+    			if (word.length > 1) {
+    				doVariant(pref + newChar, word.substring(varChar.length));
+    			}
+			});
+    	}
+	});
+}
+
+function getChar(word) {
+	
+	var newChar = "";
+	
+	transitionTable.forEach(function(variants) {
+		variants.forEach(function(variant) {
+			var ln = variant.length;	
+			if (word.substring(0,ln) == variant) {
+				if ( ln > newChar.length) {
+					newChar = variant;
+				}
+			}
+		});
+	});
+
+	return newChar;
+}
+
+var transitionTable = [
+	["a","A"], 
+	["i","I"], 
+	["u","U"], 
+	["r","R","RR"], 
+	["l","lR","lRR"],
+	["e", "ai", "o", "au"],
+	["h","H"],
+	["M","n","N","J","G"],
+	["z","S","s"],
+	["b","v"],
+	["k","kh"],
+	["g","gh"],
+	["c","ch"],
+	["j","jh"],
+	["T","Th","t","th"], 
+	["D","Dh","d","dh"],
+	["p","ph"], 
+	["b","bh"],
+	["y","r","v"]
+]
+
+function getVariants(word){
+	searchList = [];	
+	console.log(word);
+
+	doVariant("",word);
+
+	console.log(searchList.toString());
+
+	return searchList;
+}
